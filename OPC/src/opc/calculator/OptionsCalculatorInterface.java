ea0c45da6bcb176db7476d2cc9dcf6622e714d2a
stevenlovesherry <stevenlovesherry@24b072de-5b3f-11de-9579-adebd1cebf4d>
2009-06-28 12:27:04 +0000

    plain vanilla options : stock, stock index, currency, futures

---
+++ OPC/src/opc/calculator/OptionsCalculatorInterface.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package opc.calculator;
+
+/**
+ *
+ * @author user
+ */
+public interface OptionsCalculatorInterface {
+
+    
+
+    public interface LONG_SHORT
+    {
+        final String LONG = "Long";
+        final String SHORT = "Short";
+    }
+    
+    public double[][] getPlotArray();
+
+    // TODO: complete all common methods
+    
+}
---
+++ OPC/src/opc/mathalgo/OPCConstants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package opc.mathalgo;
+
+/**
+ *
+ * @author user
+ */
+public class OPCConstants {
+
+    public interface COMPOUNDING_FREQUENCY
+    {
+        final String CONTINUOUSLY = "Continuously";
+        final String ANNUALLY = "Annually";
+        final String SEMI_ANNUALLY = "Semi-annually";
+        final String QUARTERLY = "Quarterly";
+        final String MONTHLY = "Monthly";
+    }
+}
